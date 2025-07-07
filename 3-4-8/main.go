cda10421de9fbd4652670911ea4bc3abcccbc7ab
xarturino <roman.turik48@gmail.com>
2025-07-07 15:12:06 +0300

    задача по ферзю

--- 3-4-8/main.go
+++ 3-4-8/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	//TODO
+	var x1, y1, x2, y2 float64
+	fmt.Scan(&x1, &y1, &x2, &y2)
+	if ((x1 == x2 && y1 != y2) || (y1 == y2 && x1 != x2)) || (math.Abs(x2-x1) == math.Abs(y2-y1)) {
+		fmt.Print("YES")
+	} else {
+		fmt.Print("NO")
+	}
 }
